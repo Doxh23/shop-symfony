d2ed5506ae388e7c32f41ae1ce20930c0ba3189d
doxh23 <adrien.peters95@gmail.com>
2021-12-21 14:09:51 +0100

    handle add adress

--- src/Controller/AccountAdressController.php
+++ src/Controller/AccountAdressController.php
@@ -2,20 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Adress;
+use App\Form\AdressType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AccountAdressController extends AbstractController
 {
+    private $entityManager ;
+    public function __construct(entityManagerInterface $entityManager){
+            $this->entitymanager = $entityManager;
+    }
     #[Route('/compte/adresses', name: 'account_adress')]
     public function index(): Response
     {
         return $this->render('account/adress.html.twig');
     }
     #[Route('/compte/adresses/ajoutez-une-adresse', name: 'account_adress_add')]
-    public function Add(): Response
+    public function Add(Request $request): Response
     {
-        return $this->render('account/adress_add.html.twig');
+        $adress = new Adress();
+        $form = $this->createForm(AdressType::class,$adress);
+        $form->handleRequest($request);
+        if ($form ->isSubmitted() &&$form->isValid()){
+            $adress->setUser($this->getUser());
+        $this->entitymanager->persist($adress);
+            $this->entitymanager->flush();
+            return $this->redirectToRoute('account');
+
+}
+        return $this->render('account/adress_add.html.twig', [
+            "form" =>$form->createView()
+        ]);
     }
 }
--- src/Form/AdressType.php
+++ src/Form/AdressType.php
@@ -6,20 +6,68 @@ use App\Entity\Adress;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class AdressType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('company')
-            ->add('adress')
-            ->add('postal')
-            ->add('city')
-            ->add('phone')
+            ->add('name',TextType::class,[
+                'label' => 'quel nom souhaitez vous donner a votre adress',
+                'attr' => [
+                    'placeholder'=> 'nommez votre adresse'
+                ]
+            ])
+            ->add('firstname',TextType::class,[
+                'label' => 'quel est votre prénom',
+                'attr' => [
+                    'placeholder'=> 'quel est votre prenom'
+                ]
+            ])
+            ->add('lastname',TextType::class,[
+                'label' => 'quel est votre Nom',
+                'attr' => [
+                    'placeholder'=> 'quel est votre prenom'
+                ]
+            ])
+            ->add('company',TextType::class,[
+                'label' => 'votre nom de société',
+                'attr' => [
+                    'placeholder'=> 'nommez votre société'
+                ]
+            ])
+            ->add('adress',TextType::class,[
+                'label' => 'quel est votre adresse',
+                'attr' => [
+                    'placeholder'=> 'quel est  votre adresse'
+                ]
+            ])
+            ->add('postal',TextType::class,[
+                'label' => 'quel est votre code postale',
+                'attr' => [
+                    'placeholder'=> 'code postale'
+                ]
+            ])
+            ->add('city',TextType::class,[
+                'label' => 'quel est le nom de votre ville',
+                'attr' => [
+                    'placeholder'=> 'nommez votre ville'
+                ]
+            ])
+            ->add('phone',TextType::class,[
+                'label' => 'quel est votre numéro de téléphone',
+                'attr' => [
+                    'placeholder'=> 'votre numéro de téléphone'
+                ]
+            ])
+            ->add('submit',SubmitType::class,[
+                'attr' => [
+                    'placeholder'=> 'sauvegardez cette nouvelle adresse'
+                ]
+            ])
         ;
     }
 
